e7065f5e68b5e0d49c05d637f457e39dda0fb4e9
Nicholas Alvin <mystm6219@gmail.com>
2024-06-14 16:25:29 +0700

    Add files via upload

---
+++ list-siswa.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran Siswa Baru SMK N 1 Ngawi</title>
+</head>
+<body>
+    <header>
+        <h3>Siswa yang sudah mendaftar</h3>
+    </header>
+
+    <nav>
+        <a href="form-daftar.php">[+] Tambah Baru</a>
+    </nav>
+    <br>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>Jenis Kelamin</th>
+                <th>Agama</th>
+                <th>Sekolah Asal</th>
+                <th>Tindakan</th>
+            </tr>
+        </thead>
+        <tbody>
+
+                </tbody>
+    </table>
+    <p>Total: 0</p>
+    
+</body>
+</html>
